fb199838bcb9a2c299707ab59d6c5924cc9120b3
SoaringDylan <dylan@fampiera.nl>
2017-03-06 11:12:58 +0100

    Step 3

---
+++ SpaceInvadersFramework/SpaceInvadersFramework/Invader.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace SpaceInvadersFramework
+{
+    class Invader : SpriteGameObject
+    {
+        public Invader(float hposOffset, float vposOffset, string assetname, int layer = 0, string id = "", int sheetIndex = 0) : base(assetname, layer, id, sheetIndex)
+        {
+            this.Position = new Vector2((Sprite.Width * hposOffset)+(8 * hposOffset), vposOffset);
+            this.Velocity = new Vector2(2,0);
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+
+            if(this.Position.X >= (SpaceInvaders.Screen.X - Sprite.Width))
+            {
+                this.Velocity = new Vector2(-2,0);
+                this.Position -= new Vector2(0, -32);
+            }
+            if(this.position.X < 0)
+            {
+                this.Velocity = new Vector2(2, 0);
+                this.Position -= new Vector2(0, -32);
+            }
+
+            this.Position += this.Velocity;
+        }
+    }
+}
--- SpaceInvadersFramework/SpaceInvadersFramework/PlayingState.cs
+++ SpaceInvadersFramework/SpaceInvadersFramework/PlayingState.cs
@@ -7,10 +7,21 @@ namespace SpaceInvadersFramework
 {
     class PlayingState : GameObjectList
     {
+        GameObjectList invaders;
         public PlayingState()
         {
+            this.invaders = new GameObjectList(0, "invaders");
+
+            for (int i = 0; i < 9; i++)
+            {
+                this.invaders.Add(new Invader(i, 0,"blue_invader"));
+                this.invaders.Add(new Invader(i, 64, "yellow_invader"));
+                this.invaders.Add(new Invader(i, 128, "red_invader"));
+            }
+
             this.Add(new SpriteGameObject("background"));
             this.Add(new Player());
+            this.Add(invaders);
         }
     }
 }
