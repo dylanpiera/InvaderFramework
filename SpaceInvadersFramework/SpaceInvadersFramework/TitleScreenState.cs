bd7f9fdb315b9a3d6e0263a2950bbeda1a1cd3bc
SoaringDylan <dylan@fampiera.nl>
2017-03-06 11:50:41 +0100

    Time Taken to Create
    It took me about 64 minutes in a train, while chatting with a friend to create this.

--- SpaceInvadersFramework/SpaceInvadersFramework/TitleScreenState.cs
+++ SpaceInvadersFramework/SpaceInvadersFramework/TitleScreenState.cs
@@ -7,13 +7,18 @@ namespace SpaceInvadersFramework
 {
     class TitleScreenState : GameObjectList
     {
-        private TextGameObject text;
+        private TextGameObject text, time;
 
         public TitleScreenState()
         {
             this.text = new TextGameObject("GameFont");
+            this.time = new TextGameObject("GameFont");
             this.text.Text = "Press \"space\" to start the game";
-            this.text.Position = new Microsoft.Xna.Framework.Vector2((SpaceInvaders.Screen.X/2)-(this.text.Size.X/2), (SpaceInvaders.Screen.Y / 2) - (this.BoundingBox.Height / 2));
+            this.time.Text = "This game took aprox. 64 minutes to make.";
+            this.text.Position = new Microsoft.Xna.Framework.Vector2((SpaceInvaders.Screen.X/2) - (this.text.Size.X/2), (SpaceInvaders.Screen.Y / 2) - (this.text.Size.Y / 2));
+            this.time.Position = new Microsoft.Xna.Framework.Vector2((SpaceInvaders.Screen.X / 2) - (this.time.Size.X / 2), (SpaceInvaders.Screen.Y / 2) + (this.text.Size.Y/ 2));
+
+            this.Add(time);
             this.Add(text);
         }
 
